294086e9ee29c0ea7009541b72428ba8c5ef22f9
Felipe-BP <gvv.fel@gmail.com>
2022-06-06 00:04:42 -0300

    feat(example): adding more endpoints

--- src/nestjs-knex-client/nestjs-knex-client.controller.ts
+++ src/nestjs-knex-client/nestjs-knex-client.controller.ts
@@ -1,21 +1,39 @@
-import { Controller, Get, Inject } from '@nestjs/common';
-import { KNEX_CONNECTION } from '../constants';
-import Knex = require('knex');
+import { randomUUID } from 'crypto';
+import { Controller, Delete, Get, Inject, Param, Query } from '@nestjs/common';
 
+import * as Knex from 'knex';
+
+import { KNEX_CONNECTION } from '../constants';
 @Controller()
 export class NestjsKnexClientController {
     constructor(@Inject(KNEX_CONNECTION) private readonly knex: Knex) { }
 
     @Get()
     async index() {
-        const newCat = await this.knex('cats').insert({
+        await this.knex('cats').insert({
+            id: randomUUID(),
             name: 'Fred',
             age: 5,
             breed: 'tom cat',
+            is_deleted: false,
         });
 
         const cats = await this.knex.select('*').from('cats');
 
         return cats;
+    }
+
+    @Get('byname')
+    async getByName(@Query('name') catName: string) {
+        return await this.knex.table('cats')
+            .where('is_deleted', false)
+            .andWhere('name', 'like', catName);
+    }
+
+    @Delete(':catId')
+    async softDelete(@Param('catId') catId: string) {
+        return await this.knex('cats')
+            .update({ is_deleted: true })
+            .where({ id: catId });
     }
 }
